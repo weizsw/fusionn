8e4ed36ff9c62d58b073aa13ec3fe6c98a9bfb7f
weizsw <weizsw888@gmail.com>
2024-11-24 18:49:25 +0800

    feat(async_merge.go): add AsyncMergeHandler and AsyncMergePipeline to handle asynchronous merging of data in the application

---
+++ internal/handler/async_merge.go
@@ -0,0 +1,55 @@
+package handler
+
+import (
+	"fusionn/internal/model"
+	"fusionn/internal/processor"
+
+	"github.com/gin-gonic/gin"
+)
+
+type AsyncMergeHandler struct {
+	pipeline *AsyncMergePipeline
+}
+
+func NewAsyncMergeHandler(pipeline *AsyncMergePipeline) *AsyncMergeHandler {
+	return &AsyncMergeHandler{
+		pipeline: pipeline,
+	}
+}
+
+func ProvideAsyncMergePipeline(
+	parseFileStage *processor.ParseFileStage,
+	segMergeStage *processor.SegMergeStage,
+	styleStage *processor.StyleStage,
+	exportStage *processor.ExportStage,
+	subsetStage *processor.SubsetStage,
+	notiStage *processor.NotiStage,
+) *AsyncMergePipeline {
+	stages := []processor.Stage{
+		parseFileStage,
+		segMergeStage,
+		styleStage,
+		exportStage,
+		subsetStage,
+		notiStage,
+	}
+	return &AsyncMergePipeline{
+		Pipeline: processor.NewPipeline(stages...),
+	}
+}
+
+func (h *AsyncMergeHandler) AsyncMerge(c *gin.Context) error {
+	req := &model.AsyncMergeRequest{}
+	if err := c.ShouldBindJSON(req); err != nil {
+		return err
+	}
+
+	ctx := c.Request.Context()
+
+	_, err := h.pipeline.Execute(ctx, req)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
