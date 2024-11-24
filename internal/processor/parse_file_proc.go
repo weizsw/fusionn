d94bc542ec25013b7b2189ade9311ead669fbf47
weizsw <weizsw888@gmail.com>
2024-11-24 18:49:16 +0800

    feat(parse_file_proc.go): add new ParseFileStage struct and Process method to parse subtitles for a given merge request.

---
+++ internal/processor/parse_file_proc.go
@@ -0,0 +1,51 @@
+package processor
+
+import (
+	"context"
+	"fusionn/internal/model"
+	"fusionn/internal/service"
+	"fusionn/logger"
+	"fusionn/utils"
+
+	"go.uber.org/zap"
+)
+
+type ParseFileStage struct {
+	parser service.Parser
+}
+
+func NewParseFileStage(parser service.Parser) *ParseFileStage {
+	return &ParseFileStage{
+		parser: parser,
+	}
+}
+
+func (p *ParseFileStage) Process(ctx context.Context, input any) (any, error) {
+	req, ok := input.(*model.AsyncMergeRequest)
+	if !ok {
+		return nil, ErrInvalidInput
+	}
+
+	filename := utils.ExtractFilenameWithoutExtension(req.ChsSubtilePath)
+	filename = utils.RemoveLanguageExtensions(filename)
+	filePath := utils.ExtractPathWithoutExtension(req.ChsSubtilePath)
+	filePath = utils.RemoveLanguageExtensions(filePath)
+
+	logger.L.Info("[ParseFileStage] parsing subtitles", zap.String("chs_subtitle_path", req.ChsSubtilePath), zap.String("eng_subtitle_path", req.EngSubtilePath), zap.String("filename", filename), zap.String("file_path", filePath))
+	chsSub, err := p.parser.Parse(req.ChsSubtilePath)
+	if err != nil {
+		return nil, err
+	}
+
+	engSub, err := p.parser.Parse(req.EngSubtilePath)
+	if err != nil {
+		return nil, err
+	}
+
+	return &model.ParsedSubtitles{
+		FilePath:    filePath,
+		FileName:    filename,
+		ChsSubtitle: chsSub,
+		EngSubtitle: engSub,
+	}, nil
+}
