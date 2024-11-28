72b5974ce85bdba90b1268daa5713d3b50239886
weizsw <weizsw888@gmail.com>
2024-11-28 18:28:34 +0800

    feat(wire.go): add SubTrans dependency to wire module for improved functionality

--- pkg/wire.go
+++ pkg/wire.go
@@ -5,4 +5,5 @@ import "github.com/google/wire"
 var Set = wire.NewSet(
 	wire.Bind(new(DeepL), new(*deepL)), NewDeepL,
 	wire.Bind(new(Apprise), new(*apprise)), NewApprise,
+	wire.Bind(new(SubTrans), new(*subTrans)), NewSubTrans,
 )
