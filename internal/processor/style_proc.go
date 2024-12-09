8196077282b8e39276c276c1bbb1aa6f4b4cda3c
weizsw <weizsw888@gmail.com>
2024-12-09 12:13:57 +0800

    style(style_proc.go): add logic to remove punctuation from merge subtitle if translated flag is true

--- internal/processor/style_proc.go
+++ internal/processor/style_proc.go
@@ -26,5 +26,8 @@ func (s *StyleStage) Process(ctx context.Context, input any) (any, error) {
 	req.MergeSubtitle = s.styleService.AddStyle(req.MergeSubtitle)
 	req.MergeSubtitle = s.styleService.ReduceMargin(req.MergeSubtitle, "{\\org(-2000000,0)\\fr-0.00005}", "{\\org(-2000000,0)\\fr0.00015}")
 	req.MergeSubtitle = s.styleService.ReplaceSpecialCharacters(req.MergeSubtitle)
+	if req.Translated {
+		req.MergeSubtitle = s.styleService.RemovePunctuation(req.MergeSubtitle)
+	}
 	return req, nil
 }
