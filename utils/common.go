cb8ce68d5bf127926510a04113f89bd4c34d9ad3
weizsw <weizsw888@gmail.com>
2025-05-03 22:32:46 +0800

    fix(parser.go): correct variable assignment for English subtitles in ParseFromBytes function fix(common.go): expand English regex to include British and American variations for better language detection

--- utils/common.go
+++ utils/common.go
@@ -136,7 +136,7 @@ func IsCht(lan string, title string) bool {
 }
 
 func IsEng(lan string, title string) bool {
-	var englishRegex = regexp.MustCompile(`(?i)(english|eng|en|^$)`)
+	var englishRegex = regexp.MustCompile(`(?i)(english|eng|en|British|American|US|UK|America|^$)`)
 	isEngTitle := englishRegex.MatchString(title)
 	return (lan == consts.ENG_LAN) && isEngTitle
 }
